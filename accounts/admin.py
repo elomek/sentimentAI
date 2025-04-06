1f8f2cd9a9a3c611526c3668db8ed34e9d804d78
elomek <elhamkaramian90@ymail.com>
2025-04-06 21:21:30 +0200

    connect admin panel with customuser

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,17 +1,17 @@
-'''from django.contrib import admin
+from django.contrib import admin
 from accounts.models import CustomUser
 from django.contrib.auth.admin import UserAdmin
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'is_staff','is_active' ]
+
+
+    # افزودن email به بخش اضافه‌کردن یوزر
     add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("username", "usable_password","email", "password1", "password2"),
-            },
-        ),
-    
-    )'''
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
